07bb2144b335227c2d9f28eb18f579594ca929e7
Zacharia Njiraini <zjerainy@gmail.com>
2022-01-27 12:25:05 +0300

    Initial

---
+++ variables.php
@@ -0,0 +1,19 @@
+<?php
+$majina="Eric Wete";
+$MAJINA="Faith Kiruga";
+
+echo "My name is $MAJINA";
+
+$nambari1=987;
+$nambari2=563;
+
+echo "<br>";
+echo $nambari1 * $nambari2;
+echo "<br>";
+echo "The sum is :.....";
+echo "<br>";
+echo $nambari1 + $nambari2;
+echo "<br>";
+echo "The difference is :....";
+echo "<br>";
+echo $nambari1 - $nambari2;
