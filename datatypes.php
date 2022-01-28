216e74f1483e7c2f7f9687caa3d92b372cc37683
Zacharia Njiraini <zjerainy@gmail.com>
2022-01-28 10:31:04 +0300

    Added datatypes.php

---
+++ datatypes.php
@@ -0,0 +1,42 @@
+<?php
+//string
+$name="Emobilis Mobile Technology";
+echo $name;
+echo "<br>";
+
+//numbers
+$num=67.78;
+
+var_dump($name);
+echo "<br>";
+var_dump($num);
+echo "<br>";
+
+//boolean
+$me=true;
+$you=false;
+echo $me;
+echo $you;
+echo "<br>";
+
+//array
+$name=array("Erick", "Christine", "Zulfar", "Charles");
+echo $name[0];
+echo "<br>";
+
+//null
+$chair="sofa";
+$password=5522525;
+
+echo str_repeat("Hello there Explorer Class ,", 5);
+echo "<br>";
+echo strlen("My name is Zack");
+echo "<br>";
+echo str_word_count("Hello World");
+echo "<br>";
+echo str_replace("Leo", "Kesho", "Leo ni Leo");
+echo "<br>";
+echo str_split("Sasa wewe ni nani", 2);
+echo "<br>";
+echo strrev("I am a bot");
+?>
