0f27230b6504c4b903adadb8906db99e93bda3a2
Zacharia Njiraini <zjerainy@gmail.com>
2022-02-02 11:04:32 +0300

    Added loops.php and switchstatement.php

---
+++ loops.php
@@ -0,0 +1,37 @@
+<?php
+
+//While Loop
+
+$num=5; //initializes my loop
+
+while ($num<=15){ //continue repeating as long as true
+    echo "Nambari ni: $num <br>";
+    $num+=2; //increase loop by 2 for each
+}
+
+echo "<br>";
+//do...while loop
+$num2=11;
+do {
+    echo "Nambari ni: $num2 <br>";
+    $num2++;
+}while($num2<=10);
+
+echo "<br>";
+
+//While Loop
+
+$num=10; //initializes my loop
+
+while ($num<=50){ //continue repeating as long as true
+    echo "Nambari ni: $num <br>";
+    $num+=5; //increase loop by 5 for each
+}
+
+echo "<br>";
+//do...while loop
+$num2=15;
+do {
+    echo "Nambari ni: $num2 <br>";
+    $num2+=5;
+}while($num2<=50);
---
+++ switchstatement.php
@@ -0,0 +1,41 @@
+<?php
+$myfruit="Mangoes";
+
+switch ($myfruit){
+
+    case "Oranges":
+        echo "I have Oranges";
+        break;
+    case "Water Melon":
+        echo "I have Water Melon";
+        break;
+    case "Bananas":
+        echo "I have Bananas";
+        break;
+    case "Mangoes":
+        echo "I have Mangoes";
+        break;
+    default:
+        echo "No fruit in the list";
+}
+echo "<br>";
+
+$musicgenre="Blues";
+
+switch ($musicgenre){
+
+    case "Pop":
+        echo "You like Pop";
+        break;
+    case "R&B":
+        echo "You like R&B";
+        break;
+    case "Reggae":
+        echo "You like Reggae";
+        break;
+    case "Blues":
+        echo "You like Blues";
+        break;
+    default:
+        echo "Idk what kind of music you like";
+}
